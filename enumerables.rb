53345482be59e2de339f85702ea5df695c099b79
Greg0109 <greg.rabago@gmail.com>
2021-02-08 15:20:35 +0100

    Add my_each_with_index and my_select

--- enumerables.rb
+++ enumerables.rb
@@ -6,8 +6,26 @@ module Enumerables
             i += 1
         end
     end
+    def my_each_with_index(array)
+        i = 1
+        array.length.times do
+            yield(i-1,i)
+            i += 1
+        end
+    end
+    def my_select(array)
+        i = 1
+        array.length.times do
+            if yield(array[i])
+                puts array[i]
+            end
+            i += 1
+        end
+    end
 end
 
 include Enumerables
 testarray = [1,2,3,4,5,6,7,8]
 Enumerables.my_each(testarray) { |x,y| puts "The number is #{x}" }
+Enumerables.my_each_with_index(testarray) { |index,val| puts "index: #{index} for #{val}"}
+Enumerables.my_select(testarray) { |n| n.even? }
