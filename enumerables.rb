b0958f3a70ebb25e6305117e4b5fb669e733fc7f
Sergio <sbouden16@gmail.com>
2021-02-09 10:32:10 +0100

    Add linters fixing, accomodation.

--- enumerables.rb
+++ enumerables.rb
@@ -1,155 +1,116 @@
 module Enumerables
-    def my_each
-        i = 0
-        self.length.times do
-            yield(self[i])
-            i += 1
-        end
-        return self
+  def my_each
+    i = 0
+    length.times do
+      yield(self[i])
+      i += 1
     end
+    self
+  end
 
-    def my_each_with_index
-        i = 0
-        self.length.times do
-            yield(self[i],i)
-            i += 1
-        end
-        return self
+  def my_each_with_index
+    i = 0
+    length.times do
+      yield(self[i], i)
+      i += 1
     end
+    self
+  end
 
-    def my_select
-        i = 0
-        self.length.times do
-            if yield(self[i])
-                puts self[i]
-            end
-            i += 1
-        end
+  def my_select
+    i = 0
+    length.times do
+      puts self[i] if yield(self[i])
+      i += 1
     end
+  end
 
-    def my_all
-        i = 0
-        statement = true
-        self.length.times do
-            if !yield(self[i])
-                 statement = false
-            end
-            i += 1
-        end
-        return statement
+  def my_all
+    i = 0
+    statement = true
+    length.times do
+      statement = false unless yield(self[i])
+      i += 1
     end
+    statement
+  end
 
-    def my_none
-        i = 0
-        statement = false
-        if block_given?
-            self.length.times do
-                if !yield(self[i])
-                     statement = true
-                end
-                i += 1
-            end
-        else
-            self.length.times do
-                if self[i] == true
-                    return true
-                end
-            end
-        end
-        return statement
+  def my_none
+    i = 0
+    statement = false
+    if block_given?
+      length.times do
+        statement = true unless yield(self[i])
+        i += 1
+      end
+    else
+      length.times do
+        return true if self[i] == true
+      end
     end
+    statement
+  end
 
-    def my_any
-        i = 0
-        statement = false
-        if block_given?
-            self.length.times do
-                if yield(self[i])
-                     return true
-                end
-                i += 1
-            end
-        else
-            self.length.times do
-                if self[i] != false or self[i] != nil
-                    return true
-                end
-            end
-        end
-        return statement
-    end
+  def my_any
+    i = 0
+    statement = false
+    if block_given?
+      length.times do
+        return true if yield(self[i])
 
-    def my_count
-        i = 0
-        if block_given?
-            c = 0
-            self.length.times do
-                if yield(self[i])
-                    c += 1
-                end
-                i += 1
-            end
-            return c
-        else
-            self.length.times do
-                i += 1
-            end
-            return i
-        end
+        i += 1
+      end
+    else
+      length.times do
+        return true if self[i] != false or !self[i].nil?
+      end
     end
+    statement
+  end
 
-    def my_map(block=nil)
-        i = 0
-        newArray = Array.new
-        if block != nil
-            self.length.times do
-                newArray.push(block.call(self[i]))
-                i += 1
-            end
-        else
-            self.length.times do
-                newArray.push(yield(self[i]))
-                i += 1
-            end
-        end
-        return newArray
+  def my_count
+    i = 0
+    if block_given?
+      c = 0
+      length.times do
+        c += 1 if yield(self[i])
+        i += 1
+      end
+      c
+    else
+      length.times do
+        i += 1
+      end
+      i
     end
+  end
 
-    def my_inject
-        accumulator = self[0]
-        self.my_each_with_index {
-            |n,i| if i != 0
-                accumulator = yield(accumulator,n)
-            end
-        }
-        return accumulator
+  def my_map(block = nil)
+    i = 0
+    new_array = []
+    if !block.nil?
+      length.times do
+        new_array.push(block.call(self[i]))
+        i += 1
+      end
+    else
+      length.times do
+        new_array.push(yield(self[i]))
+        i += 1
+      end
     end
+    new_array
+  end
+
+  def my_inject
+    accumulator = self[0]
+    my_each_with_index do |n, i|
+      accumulator = yield(accumulator, n) if i != 0
+    end
+    accumulator
+  end
 end
 
 def multiply_els(array)
-    return array.my_inject { |sum,n| sum * n }
+  array.my_inject { |sum, n| sum * n }
 end
-
-# include Enumerables
-# testarray = [1,2,3,4,5,6,7,8]
-# testarray.my_each { |x| puts "The number is #{x}" }
-# puts
-# testarray.my_each_with_index { |val,index| puts "index: #{index} for #{val}" }
-# puts
-# testarray.my_select { |n| n.even? }
-# puts
-# puts testarray.my_all { |n| n < 20 }
-# puts
-# puts testarray.my_none { |n| n < 20 }
-# puts
-# puts testarray.my_any { |n| n == 20 }
-# puts
-# puts testarray.my_count { |n| n.even? }
-# puts
-# square = Proc.new { |x| x**2 }
-# #puts testarray.my_map { |x| x**2 }
-# puts testarray.my_map(square)
-# puts
-# puts testarray.my_inject { |sum, n| sum * n }
-# puts
-# puts multiply_els([2,4,5])
